fd9b9d487bbbd5eb0df9daea31486305d95aad6a
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-29 17:51:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,8 +45,7 @@ class Song
    song = self.new
    song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
-    song.save
-    song
+    @@all << song 
    end 
  
    
